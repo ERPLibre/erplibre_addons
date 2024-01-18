2ee59f97b363b1ccc70dbc716b5e393a54b099f2
Mathieu Benoit <mathben@technolibre.ca>
2024-01-18 00:47:46 -0500

    wip

--- erplibre_devops/models/devops_test_case_exec.py
+++ erplibre_devops/models/devops_test_case_exec.py
@@ -6,3 +6,15 @@ class DevopsTestCaseExec(models.Model):
     _description = "devops_test_case_exec"
 
     name = fields.Char()
+
+    is_finish = fields.Boolean(help="Execution is finish")
+
+    is_pass = fields.Boolean(help="True test pass, else test fail.")
+
+    log = fields.Text(help="Log for the test")
+
+    test_plan_exec_id = fields.Many2one(
+        comodel_name="devops.test.plan.exec",
+        string="Plan",
+        ondelete="cascade",
+    )
