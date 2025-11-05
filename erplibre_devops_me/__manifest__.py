46b585fa94ba91adb3239d90069c8d226cac4def
Mathieu Benoit <mathben@technolibre.ca>
2025-11-05 00:43:56 -0500

    [MIG] erplibre_devops_me: Migration to 17.0

--- erplibre_devops_me/__manifest__.py
+++ erplibre_devops_me/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "ERPLibre DevOps me",
     "category": "Tools",
     "summary": "Call other action to auto-configure me",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Mathieu Benoit",
     "license": "AGPL-3",
     "website": "https://erplibre.ca",
