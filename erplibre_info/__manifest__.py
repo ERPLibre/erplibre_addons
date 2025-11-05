95b3aea04e5dcd5dbc802c8f9d7a378b88096329
Mathieu Benoit <mathben@technolibre.ca>
2025-11-05 00:08:58 -0500

    [MIG] erplibre_info: Migration to 15.0

--- erplibre_info/__manifest__.py
+++ erplibre_info/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "ERPLibre info",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "TechnoLibre",
     "website": "https://technolibre.ca",
     "license": "AGPL-3",
