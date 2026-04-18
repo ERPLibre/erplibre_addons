551aed063d381c3f0147682316614a53e1398ad5
Mathieu Benoit <mathben@technolibre.ca>
2026-04-18 17:55:40 -0400

    [ADD] erplibre_sync_external_data_test: module skeleton

---
+++ erplibre_sync_external_data_test/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Sync External Data — Test",
+    "version": "18.0.1.0.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "category": "Tests",
+    "depends": [
+        "erplibre_sync_external_data",
+        "crm",
+        "sale",
+        "project",
+        "account",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
